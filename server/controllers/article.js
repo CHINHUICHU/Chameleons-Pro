7838982f10952a98e49424a3cb8eab0bd8f7b0fe
Chu Chin-Hui <jimmychu021@gmail.com>
2022-10-01 14:27:51 +0800

    [refactor] refactor mutiple comparison api to split sentence first

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -40,12 +40,12 @@ const comparison = async (req, res, next) => {
   const sourceToken = [];
   const targetToken = [];
 
-  for (const sentense of sourceSplit) {
-    sourceToken.push(jieba.cut(sentense));
+  for (const sentence of sourceSplit) {
+    sourceToken.push(jieba.cut(sentence));
   }
 
-  for (const sentense of targetSplit) {
-    targetToken.push(jieba.cut(sentense));
+  for (const sentence of targetSplit) {
+    targetToken.push(jieba.cut(sentence));
   }
 
   // const tagNumber = 20;
@@ -119,87 +119,86 @@ const multipleComparison = async (req, res) => {
     articleTags.push(jieba.extract(element.content, tagNumber));
   });
 
-  console.log(articleTags);
-  console.log('---------------');
+  // console.log(articleTags);
+  // console.log('---------------');
 
   const articleKeyowrds = [];
 
   articleTags.forEach((element) => {
-    const newElement = element.map((tag) => tag.keyword);
-    console.log(newElement);
+    const newElement = element.map((tag) => tag.word);
+    // console.log(newElement);
     articleKeyowrds.push(newElement);
   });
 
-  console.log(articleKeyowrds);
-  console.log('---------------');
-  const articleNumber = articles.length;
-  console.log(articleNumber);
+  // console.log(articleKeyowrds);
+  // console.log('---------------');
+  // const articleNumber = articles.length;
+  // console.log(articleNumber);
+
+  const splitArticles = [];
 
   const filteredArticles = [];
+  const synonymizedArticles = [];
 
-  const synonymiedArticles = await Promise.all(
-    articles.map(async (element) => {
-      const tokenizedArticle = jieba.cut(element.content);
-      const filteredArticle = await filterStopWords(tokenizedArticle);
-      filteredArticles.push(filteredArticle);
-      const synonymiedArticle = await findSynonym(filteredArticle);
-      return synonymiedArticle;
-    })
-  );
+  for (const article of articles) {
+    const splitArticle = article.content.split(/(?:，|。|\n|！|？|：|；)+/);
 
-  const esBulkBody = [];
-  for (let i = 0; i < articleNumber; i += 1) {
-    esBulkBody.push({
-      index: {
-        _index: 'test_articles',
-      },
-    });
-    esBulkBody.push({
-      title: articles[i].title,
-      author: articles[i].author,
-      tag: articleKeyowrds[i],
-      filtered_content: filteredArticles[i],
-      processed_content: synonymiedArticles[i],
-      content: articles[i].content,
-    });
+    splitArticles.push(splitArticle);
+
+    const tokenizedArticle = splitArticle.map((sentence) =>
+      jieba.cut(sentence)
+    );
+
+    const filteredArticle = filterStopWords(tokenizedArticle);
+    filteredArticles.push(filteredArticle);
+    const synonymizedArticle = findSynonym(filteredArticle);
+    synonymizedArticles.push(synonymizedArticle);
   }
 
-  const responseFromES = await insertMultipleArticles(esBulkBody);
+  // const esBulkBody = [];
+  // for (let i = 0; i < articleNumber; i += 1) {
+  //   esBulkBody.push({
+  //     index: {
+  //       _index: 'test_articles',
+  //     },
+  //   });
+  //   esBulkBody.push({
+  //     title: articles[i].title,
+  //     author: articles[i].author,
+  //     tag: articleKeyowrds[i],
+  //     filtered_content: filteredArticles[i],
+  //     processed_content: synonymiedArticles[i],
+  //     content: articles[i].content,
+  //   });
+  // }
 
-  console.log('response from elasticSearch!!');
-  console.log(responseFromES);
-  responseFromES.items.forEach((ele) => console.log(ele));
+  // const responseFromES = await insertMultipleArticles(esBulkBody);
+
+  // console.log('response from elasticSearch!!');
+  // console.log(responseFromES);
+  // responseFromES.items.forEach((ele) => console.log(ele));
 
   const articlesGraph = Graph();
   // const matchedArticles = [];
   const matchedArticles = {};
+
+  const articleNumber = articles.length;
+
   for (let i = 0; i < articleNumber; i += 1) {
     for (let j = i + 1; j < articleNumber; j += 1) {
       const similarity = calculateSimilarity(
-        synonymiedArticles[i],
-        synonymiedArticles[j]
+        synonymizedArticles[i].flat(),
+        synonymizedArticles[j].flat()
       );
       articlesGraph.addEdge(i + 1, j + 1, similarity);
-      const [matchedArticleA, matchedArticleB] = findMatchedKeyword(
-        synonymiedArticles[i],
-        synonymiedArticles[j],
-        filteredArticles[i],
-        filteredArticles[j]
+      const matchResult = findMatchedKeyword(
+        splitArticles[i],
+        splitArticles[j],
+        synonymizedArticles[i],
+        synonymizedArticles[j]
       );
 
-      const matchedArticleAindices = findSimilarSentenseIndex(
-        articles[i].content,
-        matchedArticleA
-      );
-      const matchedBrticleAindices = findSimilarSentenseIndex(
-        articles[j].content,
-        matchedArticleB
-      );
-
-      matchedArticles[`${i + 1}-and-${j + 1}`] = [
-        matchedArticleAindices,
-        matchedBrticleAindices,
-      ];
+      matchedArticles[`${i + 1}-and-${j + 1}`] = matchResult;
     }
   }
 
@@ -209,7 +208,7 @@ const multipleComparison = async (req, res) => {
 
   const response = {
     similarity: articlesGraph.serialize(),
-    sentenseIndex: matchedArticles,
+    matchResult: matchedArticles,
   };
 
   res.send({ data: response });
@@ -241,15 +240,15 @@ const getArticles = async (req, res) => {
 
   const searchResult = await searchArticles(page, pageSize, esSearchQuery);
 
-  // console.log(searchResult.hits.hits);
+  console.log(searchResult.hits.hits);
 
-  // const minHashArray = [];
-  // for (let i = 0; i < searchResult.hits.hits.length; i += 1) {
-  //   minHashArray[i] = new Minhash();
-  //   searchResult.hits.hits[i]._source.processed_content.map((w) => {
-  //     minHashArray[i].update(w);
-  //   });
-  // }
+  const minHashArray = [];
+  for (let i = 0; i < searchResult.hits.hits.length; i += 1) {
+    minHashArray[i] = new Minhash();
+    searchResult.hits.hits[i]._source.processed_content.map((w) => {
+      minHashArray[i].update(w);
+    });
+  }
 
   const searchArticleResult = searchResult.hits.hits.map((element) => ({
     id: element._id,
@@ -262,6 +261,7 @@ const getArticles = async (req, res) => {
     article: searchArticleResult,
   };
   res.send({ data: response });
+  // res.send('ok');
 };
 
 const analyzeArticle = async (req, res) => {
