5759730aa96c45f16317e578c30afbe69ab1bb8d
Chu Chin-Hui <jimmychu021@gmail.com>
2022-10-06 17:03:09 +0800

    [feat] construct stop word map and synonym map with class

--- util/util.js
+++ util/util.js
@@ -2,11 +2,7 @@
 require('dotenv').config();
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const _ = require('lodash');
 const { getUserProfile } = require('../server/models/user');
-
-const readFileAsync = promisify(fs.readFile);
 
 const authentication = async (req, res, next) => {
   let accessToken = req.get('Authorization');
@@ -42,71 +38,6 @@ const authentication = async (req, res, next) => {
 const wrapAsync = (fn) => (req, res, next) => {
   fn(req, res, next).catch(next);
 };
-
-async function buildStopWordsMap() {
-  const stopwordMap = new Map();
-  const stopWords = (await readFileAsync('./stops.utf8'))
-    .toString()
-    .split('\n');
-  for (const word of stopWords) {
-    stopwordMap.set(word, -1);
-  }
-  stopwordMap.set(' ', -1);
-  stopwordMap.set('\n', -1);
-  return stopwordMap;
-}
-
-let stopWordMap = new Map();
-let synonymMap = new Map();
-
-async function buildSynonymMap() {
-  // build synonym map
-  // const synonymMap = new Map();
-  const synonyms = (await readFileAsync('./dict_synonym.txt'))
-    .toString()
-    .split('\n');
-  for (const sysnonym of synonyms) {
-    const words = sysnonym.split(' ');
-    for (let i = 1; i < words.length; i += 1) {
-      synonymMap.set(words[i], words[0]);
-    }
-  }
-  return synonymMap;
-}
-
-async function main() {
-  stopWordMap = await buildStopWordsMap();
-  synonymMap = await buildSynonymMap();
-}
-
-main();
-
-function filterStopWords(splitedParagraphArray) {
-  const result = [];
-  const newArray = _.cloneDeep(splitedParagraphArray);
-  for (const sentence of newArray) {
-    for (let i = 0; i < sentence.length; i += 1) {
-      if (stopWordMap.get(sentence[i])) {
-        sentence[i] = -1;
-      }
-    }
-    const sentenceResult = sentence.filter((ele) => ele !== -1);
-    result.push(sentenceResult);
-  }
-  return result;
-}
-
-function findSynonym(paragraphArray) {
-  const newParagraphArray = _.cloneDeep(paragraphArray);
-  for (const sentence of newParagraphArray) {
-    for (let i = 0; i < sentence.length; i += 1) {
-      if (synonymMap.get(sentence[i])) {
-        sentence[i] = synonymMap.get(sentence[i]);
-      }
-    }
-  }
-  return newParagraphArray;
-}
 
 function findMatchedKeyword(
   articleA,
@@ -172,8 +103,6 @@ function calculateSimilarity(articleA, articleB) {
 module.exports = {
   authentication,
   wrapAsync,
-  filterStopWords,
-  findSynonym,
   findMatchedKeyword,
   calculateSimilarity,
 };
