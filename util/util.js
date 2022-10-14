bac1c1d6036358a6f3f525324e2ebf963cb0c804
Chu Chin-Hui <jimmychu021@gmail.com>
2022-10-14 21:38:48 +0800

    [feat] separate auth for app and socket

--- util/util.js
+++ util/util.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { getUserProfile } = require('../server/models/user');
 const { promisify } = require('util');
 
-const authentication = async (req, res, next) => {
+const appAuth = async (req, res, next) => {
   let accessToken = req.get('Authorization');
   if (accessToken === 'null' || !accessToken) {
     return res.status(401).send({
@@ -33,11 +33,48 @@ const authentication = async (req, res, next) => {
   }
 };
 
+const socketAuth = async (socket, next) => {
+  let accessToken = socket.handshake.auth.token;
+
+  if (accessToken === 'null' || !accessToken) {
+    const err = new Error('not authorized');
+    err.data = { status_code: 401, message: 'please sign in or sign up' };
+    return next(err);
+  }
+
+  accessToken = accessToken.replace('Bearer ', '');
+
+  try {
+    const user = await promisify(jwt.verify)(
+      accessToken,
+      process.env.SECRET_TOKEN
+    );
+
+    const result = await getUserProfile(user);
+
+    socket.user = {
+      user_id: result.data.hits.hits[0]._id,
+      name: result.data.hits.hits[0]._source.name,
+      email: user.email,
+    };
+
+    socket.join(socket.user.user_id);
+
+    return next();
+  } catch (err) {
+    const error = new Error('wrong token');
+    error.data = { status_code: 403, message: 'the token is wrong' };
+
+    return next(error);
+  }
+};
+
 const wrapAsync = (fn) => (req, res, next) => {
   fn(req, res, next).catch(next);
 };
 
 module.exports = {
-  authentication,
+  appAuth,
+  socketAuth,
   wrapAsync,
 };
