a1cda1e8acb5377f6b1d004989472aed950e7691
Chu Chin-Hui <jimmychu021@gmail.com>
2022-10-14 21:37:09 +0800

    [refactor] reshape error message and response format

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,7 +8,9 @@ const signUp = async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ error: errors.array() });
+    return res
+      .status(400)
+      .json({ status_code: 400, message: errors.array()[0].msg });
   }
 
   const result = await User.signUp(user);
@@ -21,7 +23,9 @@ const signIn = async (req, res) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ error: errors.array() });
+    return res
+      .status(400)
+      .json({ status_code: 400, message: errors.array()[0].msg });
   }
   const result = await User.signIn(user);
   return res.status(result.status_code).send(result);
